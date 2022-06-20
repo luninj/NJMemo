0077e8669a8f358d39a957b7c142d5733694dd84
luninj <103933747+luninj@users.noreply.github.com>
2022-06-20 23:14:08 +0900

    Memo Class 구현

---
+++ NJMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  NJMemo
+//
+//  Created by JunyeongChoi on 2022/06/20.
+//
+
+import Foundation
+
+class Memo {
+    var content : String
+    var insertDate : Date
+    
+    init(content : String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "init"),
+        Memo(content: "NJ Memo")
+    ]
+}
