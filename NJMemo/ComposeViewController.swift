65f86c3820362a04136e36fa2af9358c4276483a
luninj <103933747+luninj@users.noreply.github.com>
2022-06-24 22:36:26 +0900

    새 메모 화면 구현1

---
+++ NJMemo/ComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ComposeViewController.swift
+//  NJMemo
+//
+//  Created by JunyeongChoi on 2022/06/24.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
